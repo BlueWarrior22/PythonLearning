77a0eea2a97f5c3b18c33b879c11137f7cf96735
winscarcamo <carcamo.winston.89@gmail.com>
2025-02-20 09:32:41 -0500

    Commit #1

---
+++ Modules/Exercises.py
@@ -0,0 +1,22 @@
+'''Write a function that generates a six-digit/character randon_user_id'''
+
+'''from random import randint
+
+def randon_user_id():
+    user_id_str = str()
+    for i in range(5):
+        user_id_str += str(randint(0,9))
+    return user_id_str
+
+print(randon_user_id())'''
+
+
+from random import randint
+
+def genuserid ():
+    useridramdon = str()
+    for i in range (6):
+        useridramdon += str(randint(0,9))
+    return str(useridramdon)
+
+print(genuserid())
