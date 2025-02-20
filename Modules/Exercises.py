d2edd5d8a5a18bb5ef506f5c1d34f479e7a68d5c
winscarcamo <carcamo.winston.89@gmail.com>
2025-02-20 10:32:00 -0500

    Commit #3

--- Modules/Exercises.py
+++ Modules/Exercises.py
@@ -1,17 +1,16 @@
 '''Write a function that generates a six-digit/character randon_user_id'''
 
-from random import randint
+from random import random as r, randint
+import string
 
 def randon_user_id():
     user_id_str = str()
-    for i in range(5):
+    for i in range(6):
         user_id_str += str(randint(0,9))
     return user_id_str
 
 print(randon_user_id())
 
-
-from random import randint
 
 def genuserid ():
     useridramdon = str()
@@ -20,3 +19,18 @@ def genuserid ():
     return str(useridramdon)
 
 print(genuserid())
+
+
+import random
+import string
+
+#Select 2 digits at random:
+
+def randonuserid ():
+
+    digits = random.choices(string.digits, k=10)
+    letter = random.choices(string.ascii_letters, k=10)
+    randonuseridresult = ''.join(random.sample(digits+letter, 6))
+    return randonuseridresult
+
+print(randonuserid())
