bd626a114067e9376b111990b95a88a673c9801d
new name <carcamo.winston.89@gmail.com>
2025-02-21 10:23:04 -0500

    Commit #7

---
+++ Modules/Exercise-2.py
@@ -0,0 +1,15 @@
+'''
+Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array 
+(six hexadecimal numbers written after #. Hexadecimal numeral system is made out of 16 symbols, 
+0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+
+'''
+
+import random
+import string
+
+def list_of_hexa_colors ():
+    hexa_color = ''.join(random.choices(string.hexdigits, k=6))
+    return print(f'#{hexa_color}')
+
+list_of_hexa_colors()
